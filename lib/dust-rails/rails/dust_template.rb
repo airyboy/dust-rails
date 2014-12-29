abe971574ff4909be656ca639f10496ade062bee
airyboy <clevermik@gmail.com>
2014-12-30 01:16:11 +0300

    Update dust_template.rb

--- lib/dust-rails/rails/dust_template.rb
+++ lib/dust-rails/rails/dust_template.rb
@@ -30,7 +30,7 @@ module Dust
 
       def evaluate(scope, locals, &block)
         template_root = Dust.config.template_root
-        template_name = file.split(template_root).last.split('.',2).first
+        template_name = name
         Source.context.call("dust.compile", data, template_name)
       end
     end
